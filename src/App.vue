03f2cb6d2f7a1246bfd280137708f0ef2bcc4bb5
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-12 06:50:24 +0700

    #9 - Bug Fix error codes

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,14 @@
 <template>
-    <div id="app">
-        <img alt="Vue logo" src="./assets/logo.png">
-        <HelloWorld msg="Welcome to Your Vue.js App"/>
-    </div>
-</template>
-
-<script>
-    import HelloWorld from './components/HelloWorld.vue'
-
-    export default {
-        name: 'App',
-        components: {
-            HelloWorld
-        }
-    }
-</script>
-
-<style>
-    #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
-</style>
-
-<template>
     <div>
+        <form @submit.prevent="add">
+            <input type="hidden" v-model="form.id">
+            <input type="text" v-model="form.name">
+            <button type="submit" v-show="!updateSubmit">Add</button>
+            <!-- jika tidak update maka tombol update tidak muncul -->
+            <button @click="update(form)" type="button" v-show="updateSubmit">Update</button>
+            <!-- jika tombol edit di klik maka tombol add akan berubah menjadi update -->
+        </form>
         <ul :key="user.id" v-for="user in users">
-            <li>
-                <span>{{user.name}}</span> &#160;
-            </li>
             <li>
                 <span>{{user.name}}</span> &#160;
                 <button @click="edit(user)">Edit</button>
@@ -43,14 +19,6 @@
     </div>
 </template>
 
-<form @submit.prevent="add">
-    <input type="hidden" v-model="form.id">
-    <input type="text" v-model="form.name">
-    <button type="submit" v-show="!updateSubmit">add</button> <!-- jika tidak update maka tombol update tidak muncul -->
-    <button @click="update(form)" type="button" v-show="updateSubmit">Update</button>
-    <!-- jika tombol edit di klik maka tombol add akan berubah menjadi update -->
-</form>
-
 <script>
     /* eslint-disable */
     import axios from 'axios'
@@ -58,7 +26,12 @@
     export default {
         data() {
             return {
-                users: []
+                form: {
+                    id: '',
+                    name: ''
+                },
+                users: '',
+                updateSubmit: false
             }
         },
         mounted() {
